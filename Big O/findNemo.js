1d9532a86fd770a0e01756ff9c2ebc8a89853327
Muath <moath.omarsa@gmail.com>
2021-12-25 21:47:37 +0200

    find nemo in milliSeconds

--- Big O/findNemo.js
+++ Big O/findNemo.js
@@ -1,15 +1,17 @@
+const {performance} = require('perf_hooks');
+
 const names = ['moath', 'omar', 'ahmed', 'khaled', 'hasan', 'nemo', 'zezo', 'jaber'];
 
 function findNemo(array){
-    let start = process.uptime();
+    let start =  performance.now();
     for(let i= 0; i < array.length; i++){
         if (array[i] == 'nemo'){
             console.log('hey we found nemo !!')
         }
     }
-    let end = process.uptime();
+    let end =  performance.now();
     let time = end - start;
-    console.log('we have found nemo in ' + time + ' microSeconds');
+    console.log('we have found nemo in ' + time + ' milliSeconds');
 }
 
 
