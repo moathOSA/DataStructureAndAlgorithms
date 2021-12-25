cfd0a6b356aaca5ce8f0b5276ccb7be5c82bccd3
Muath <moath.omarsa@gmail.com>
2021-12-25 21:10:15 +0200

    initial commit

---
+++ Big O.js
@@ -0,0 +1,12 @@
+const nemo = ['nemo'];
+
+function findNemo(array){
+    for(let i= 0; i < array.length; i++){
+        if (array[i] == 'nemo'){
+            console.log('hey we found nemo !!')
+        }
+    }
+}
+
+
+findNemo(nemo);
