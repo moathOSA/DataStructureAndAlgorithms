9c80ad6f61dd8d44a02770412890c58828f88af7
Muath <moath.omarsa@gmail.com>
2021-12-31 23:11:15 +0200

    mergeSortedArrays

---
+++ Array/mergeSortedArrays.js
@@ -0,0 +1,45 @@
+function mergeSortedArrays(array1, array2){
+
+    isValidArrays(array1,array2);
+    
+    
+    if(array1.length === 0) {
+        return array2;
+    }
+
+    if(array2.length === 0) {
+        return array1;
+    }
+
+    const mergedArray = [];
+    let array1Item = array1[0];
+    let array2Item = array2[0];
+    let i = 1;
+    let j = 1;
+      
+
+    while (array1Item || array2Item){
+        if(array2Item === undefined || array1Item < array2Item){
+          mergedArray.push(array1Item);
+          array1Item = array1[i];
+          i++;
+        }   
+        else {
+          mergedArray.push(array2Item);
+          array2Item = array2[j];
+          j++;
+        }
+       }
+       console.log(mergedArray);
+       return mergedArray;
+  }
+
+  function isValidArrays(array1,array2){
+
+    if(array1.length === 0 && array2.length === 0){
+        //console.log("Ooch, your arrays are empty !");
+        return "Ooch, your arrays are empty !";
+    }
+}
+  
+  mergeSortedArrays([0,3,4,31], [3,4,6,30]);
