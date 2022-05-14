e7c71a4355914f80a6ed6db496d2ec02296d0904
Muath <moath.omarsa@gmail.com>
2022-05-14 06:56:29 +0300

    handle item not fount case

--- Search Algo/Binary Search/implementation.js
+++ Search Algo/Binary Search/implementation.js
@@ -1,8 +1,13 @@
 const items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-let target = 14;
+let target = 30;
 
 function binarySearch(L,R){
-    let mid = (L + R)/2;
+    if(L == R){
+        return false;
+    }
+
+    let mid = Math.floor((L + R)/2);
+
     if(mid == target){
         return target;
     }
