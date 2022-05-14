8d6f40375822390736bc27288597bbe164bead10
Muath <moath.omarsa@gmail.com>
2022-05-14 05:32:08 +0300

    implement binary search

---
+++ Search Algo/Binary Search/implementation.js
@@ -0,0 +1,19 @@
+const items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let target = 14;
+
+function binarySearch(L,R){
+    let mid = (L + R)/2;
+    if(mid == target){
+        return target;
+    }
+
+    if(mid > target){
+        binarySearch(L,mid);
+    }
+
+    if(mid < target){
+        binarySearch(mid+1,R);
+    }
+}
+
+binarySearch(1,items.length);
